bfafd9e75ee0959a42a11db38da7ff7c4d210834
manishkumawat4113-art <manishkumawat4113@gmail.com>
2026-07-30 11:03:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ const MessageSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now    
-  }
+  },
 status: {
     type: String,
     enum: ["sent", "delivered", "read"],

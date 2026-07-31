59d632885e028b3605651445c5da4261f4633f05
manishkumawat4113-art <manishkumawat4113@gmail.com>
2026-07-31 16:41:32 +0530

    Remove lastMessageStatus and lastMessageSenderId

--- server.js
+++ server.js
@@ -870,8 +870,7 @@ lastMessage:msg.text,
   
 
 lastMessageTime:msg.createdAt,
-  lastMessageStatus: msg.status || "sent",
-lastMessageSenderId: String(msg.senderId),
+  
 
 isOnline:user?.isOnline || false,
 

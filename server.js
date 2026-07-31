000ac2ab978cf2e8bd0d798a9548e6f1f6126e44
manishkumawat4113-art <manishkumawat4113@gmail.com>
2026-07-31 09:31:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -819,6 +819,10 @@ username:user?.username || "Unknown",
 profilePhoto:user?.profilePhoto || "",
 
 lastMessage:msg.text,
+  lastMessageStatus:msg.status,
+
+lastMessageSenderId:String(msg.senderId),
+  
 
 lastMessageTime:msg.createdAt,
 
